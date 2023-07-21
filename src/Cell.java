d83ae584c68119fb51410dfad673aa4ce6b8b815
eduardo661 <73264386+eduardo661@users.noreply.github.com>
2023-07-20 22:04:37 -0700

    added draw world functionality

---
+++ src/Cell.java
@@ -0,0 +1,34 @@
+import java.awt.*;
+
+public class Cell extends Rectangle {
+    private Color color = Color.BLACK;
+    private int id;
+    private boolean spider;
+    private Rectangle rect;
+
+    Cell(Color color, int id, boolean spider, int x, int y, int width, int height){
+        this.color = color;
+        this.id = id;
+        if(this.id == 0){this.spider = true;}
+        else{this.spider = false;}
+        this.rect = new Rectangle(x, y, width, height);
+    }
+
+    public Color getColor() {
+        return color;
+    }
+    public Rectangle getRect(){
+        return this.rect;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+
+    public boolean getSpider(){
+        return this.spider;
+    }
+
+
+
+}
---
+++ src/Target.java
@@ -0,0 +1,6 @@
+import java.awt.*;
+
+public class Target extends Rectangle{
+    private Color color;
+
+}
