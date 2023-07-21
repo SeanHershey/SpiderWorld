e323106028cc2d046f52bef2c8ca05ca508936ab
eduardo661 <73264386+eduardo661@users.noreply.github.com>
2023-07-21 16:03:20 -0700

    added grid to data source

--- src/DataSource.java
+++ src/DataSource.java
@@ -1,8 +1,13 @@
+import java.awt.*;
+import java.util.ArrayList;
 import java.util.Stack;
 
 public class DataSource {
     private static DataSource instance;
     Stack<Block> blocks = new Stack<Block>();
+    ArrayList<ArrayList<Cell>> grid = new ArrayList<>();
+
+
 
     private DataSource() { }
 
@@ -15,6 +20,19 @@ public class DataSource {
 
     public void addBlock(int x, int y, String type) {
         blocks.add(new Block(x, y, type));
+    }
+
+    public void setGrid(int rows, int columns){
+        for(int i = 0; i < rows; i++) {
+            ArrayList<Cell> row = new ArrayList<>();
+            for(int j = 0; j < columns; j++) {
+                row.add( new Cell(Color.BLACK, (i * 5) + j, false, 20 + (50*j), 120 + (50 * i), 48, 48 ));
+            }
+            grid.add(row);
+    } }
+
+    public ArrayList<ArrayList<Cell>> getGrid(){
+        return this.grid;
     }
 
     public Stack<Block> getBlockList(){
